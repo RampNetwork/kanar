72521433e6925800bcaa93230122711fbb9db4f5
Karol Waszczuk <karol.waszczuk@ramp.network>
2024-10-10 14:52:15 +0200

    feat: migrate logic from ClickUp to Jira #PLTM-233 (#12)
    * chore: remove ClickUp logic
    
    * feat: logic for Jira issues
    
    * refactor: simplify task reference parsing
    
    * fix: typo
    
    * feat: ignore duplicates and return tasks in alphabetical order
    
    * fix: promise not awaited
    
    * fix: invalid Jira request auth and response parsing
    
    * feat: search just the commit title for issue references
    
    * docs: change package.json description
    
    * docs: update README.md
    
    * fix: remove references to tickets in description
    
    * cleanup: remove redundant join operation
    
    Co-authored-by: Artur Kozak <artur.kozak2@gmail.com>
    
    * feat: do not sort tasks alphabetically
    
    * feat: match issue references only in the end of the title
    
    * test: adjust tests
    
    * chore: add information about placing issue references at the end of title
    
    * feat: use Github variable for token's user
    
    ---------
    
    Co-authored-by: Artur Kozak <artur.kozak2@gmail.com>

--- README.md
+++ README.md
@@ -2,14 +2,13 @@
 ![CliCop logo](clicop.png)
 
 
-CliCop is an Github Actions action that enforces pinning ClickUp ticket id reference in a PR.\
-I will fail if there is no task id in PR title or body.\
-It does not yet check if the task really exists in ClickUp.\
+CliCop is an Github Actions action that enforces pinning ticket id reference in a PR.\
+It will fail if there is no task id in PR title.\
 It utilizes [DangerJS](https://danger.systems/js/) to perform the check.
 
 ## Inputs
 github_token - Github authentification token.
-clickup_token - ClickUp authentification token.
+jira_token - Jira authentification token.
 
 ## Testing
 The action conitains some unit tests. To run them:
@@ -42,5 +41,5 @@ jobs:
       - uses: RampNetwork/github-actions/clicop@clicop-v1.0.0
         with:
           github_token: ${{ secrets.GITHUB_TOKEN }} # this is passed automatically https://docs.github.com/en/actions/security-guides/automatic-token-authentication
-          clickup_token: ${{ secrets.CLICKUP_TOKEN }}
+          jira_token: ${{ secrets.JIRA_TOKEN }}
 ```
--- dangerfile.js
+++ dangerfile.js
@@ -2,103 +2,69 @@ const axios = require('axios');
 
 const getTasks = require('./lib/get-tasks.js');
 
-const CLICKUP_API_URL = 'https://api.clickup.com/api/v2';
-const CLICKUP_TEAM_ID = '24301226';
+const JIRA_BASE_URL = 'https://rampnetwork.atlassian.net';
+const JIRA_API_URL = `${JIRA_BASE_URL}/rest/api/2`;
 
-const clickupRequest = async ({ resource, method = 'POST', data = {} }) =>
+const jiraRequest = async ({ resource, method = 'GET', data = {} }) =>
   axios({
     method,
     url: resource,
-    baseURL: CLICKUP_API_URL,
-    headers: {
-      Authorization: process.env.CLICKUP_TOKEN,
-      'Content-Type': 'application/json',
+    baseURL: JIRA_API_URL,
+    auth: {
+      username: process.env.JIRA_TOKEN_USER,
+      password: process.env.JIRA_TOKEN,
     },
     data,
-  }).catch(
-    function (error) {
-      console.log(`Error getting ${resource}`)
-    }
-  );
+  }).catch(error => {
+    console.error(`Error getting ${resource} - ${error}`)
+  });
 
-const getClickupTicketName = async (taskId, isCustom) => {
-  const resource = getTaskResource({ taskId, isCustom }, "")
+const getJiraIssueName = async (issueId) => {
+  const resource = `/issue/${issueId}?fields=summary`
 
-  const response = await clickupRequest({
+  const response = await jiraRequest({
     resource,
     method: 'GET',
   });
-  return response?.data?.name ?? undefined
+  return response?.data?.fields?.summary ?? undefined
 }
-
-const getTaskResource = ({ taskId, isCustom }, field = '') => {
-  const customQuery = isCustom ? `?custom_task_ids=true&team_id=${CLICKUP_TEAM_ID}` : '';
-  return `/task/${taskId}${field}${customQuery}`
-}
-
-const addClickupRefComment = async (taskId, isCustom) => {
-  const resource = getTaskResource({ taskId, isCustom }, '/comment')
-  const text = `This issue is referenced in ${danger.github.pr.html_url}`;
-
-  const {
-    data: { comments },
-  } = await clickupRequest({
-    resource,
-    method: 'GET',
-  });
-
-  const hasRefComment = comments.find(({ comment_text }) =>
-    comment_text.includes(text)
-  );
-
-  if (hasRefComment) {
-    return;
-  }
-
-  await clickupRequest({
-    resource,
-    data: {
-      comment: [
-        {
-          text,
-        },
-      ],
-    },
-  });
-};
 
 const parallelRequests = (tasks = [], req) => {
   if (tasks.length === 0) {
-    return[];
+    return [];
   }
 
   return Promise.all(tasks.map(req));
 };
 
-const checkAndUpdateClickupIssues = async () => {
-  const source = [danger.github.pr.title, danger.github.pr.body].join(' ');
+const checkTasks = async () => {
+  const source = danger.github.pr.title;
   const tasks = getTasks(source);
-  if (tasks.length === 0) {
+  const allTasks = await parallelRequests(tasks, async ({ taskId }) => {
+    return {
+      taskId: taskId,
+      name: await getJiraIssueName(taskId),
+    }
+  });
+
+  const tasksWithName = allTasks.filter(({ name }) => name);
+  if (tasksWithName.length === 0) {
     fail(
-      '<b>Please add the issue key to the PR e.g.: #28zfr1a or #DATAENG-98</b>\n' +
-      '(remember to add hash)\n\n' +
-      '<i>You can find ticket key eg. in the last part of URL when ticket is viewed in the browser eg.:\n' +
-      'URL: https://app.clickup.com/t/28zfr1a -> ticket issue key: 28zfr1a -> what should be added to PR: #28zfr1a\n' +
-      'URL: https://app.clickup.com/t/24301226/DATAENG-98 -> ticket issue key: DATAENG-98 -> what should be added to PR: #DATAENG-98\n\n' +
-      'You can add more than one ticket issue key in the PR title or/and description.</i>'
+      '<b>Please add the Jira issue key at the end of PR title e.g.: #DATA-98</b> (remember to add hash)\n\n' +
+      '<i>You can find issue key eg. in the last part of URL when issue is viewed in the browser eg.:\n' +
+      `URL: ${JIRA_BASE_URL}/browse/DATA-98 -> issue key: DATA-98 -> what should be added to the PR title: #DATA-98\n\n` +
+      'You can add more than one issue key in the PR title.</i>'
     );
     return;
   }
 
   message(
-    'Ticket(s) related to this PR:\n\n' +
-    tasks
-      .map(
-        ({ taskId }) =>
-          `- :link: #${taskId}`
-      )
-      .join('\n')
+    'Jira issue(s) related to this PR:\n' +
+    tasksWithName.map(
+      ({ taskId, name }) =>
+        `+ :link: <a href="${JIRA_BASE_URL}/browse/${taskId}">${name} [#${taskId}]</a>`
+    ).join('\n')
   );
 };
 
-checkAndUpdateClickupIssues();
+checkTasks();
